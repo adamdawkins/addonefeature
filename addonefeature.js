d644634da9b47f40945d34f86179a3b430ddf3a5
t-lemaire <terry.lemaire@gmail.com>
2013-11-14 20:51:06 -0500

    New handlebar helper function go get the user's first e-mail. Creator e-mail now showing in task list.

--- addonefeature.js
+++ addonefeature.js
@@ -15,12 +15,34 @@ if (Meteor.isClient) {
         title: $('#new_task_title').val(),
         description: $('#new_task_description').val(),
         creator_id: Meteor.userId(),
-        assignee_id: ''
+        assignee_id: ''//$('#assigned_user_id option:selected').val()
       });
 
       return false;
     }
   };
+}
+
+if(Meteor.isClient)
+{
+	Handlebars.registerHelper('getUserEmail', function(userId){//Allowing Handlebar to get the user's first e-mail from Meteor. (We can add an index/selector parameter later.)
+		if(userId=='')
+		{
+			return 'Undefined';
+		}
+
+		userElement = Meteor.users.findOne({'_id': userId});
+		userEmail = userElement.emails[0].address;
+
+		if(typeof userEmail=="undefined")
+		{
+			return 'Undefined';
+		}
+		else
+		{
+			return userEmail;
+		}
+	});
 }
 
 if (Meteor.isServer) {
